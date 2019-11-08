c16cb708c08553f004538dd90c8c7d2486c3622e
iperrilles <isaac-perrilles@uiowa.edu>
2019-11-08 01:04:20 -0600

    Working Data Table

--- app.py
+++ app.py
@@ -1,24 +1,47 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+import secrets
+
+conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(secrets.dbuser,secrets.dbpass,secrets.dbhost,secrets.dbname)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SuperSecretKey'
+app.config['SQLALCHEMY_DATABASE_URI'] = conn
+db = SQLAlchemy(app)
+
+class iperrilles_landmarksapp(db.Model):
+    LandmarkID = db.Column(db.Integer, primary_key=True)
+    LandmarkName = db.Column(db.String(255))
+    City = db.Column(db.String(255))
+    Country = db.Column(db.String(255))
+
+    def __repr__(self):
+        return "id: {0} | Landmark Name: {1} | City: {2} | Country: {3}".format(self.LandmarkID, self.LandmarkName, self.City, self.Country)
 
 class LandmarksForm(FlaskForm):
     Landmark_Name = StringField('Landmark Name:', validators=[DataRequired()])
+    City = StringField('City:', validators=[DataRequired()])
+    Country = StringField('Country:', validators=[DataRequired()])
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', display="", pageTitle='Landmarks')
+    all_landmarks = iperrilles_landmarksapp.query.all()
+    return render_template('index.html', landmarks = all_landmarks, pageTitle='Landmarks')
 
 @app.route('/add_Landmark', methods=['GET', 'POST'])
 def add_Landmark():
     form = LandmarksForm()
     if form.validate_on_submit():
-        return "<h2> The Landmark is {0}".format(form.Landmark_Name.data)
+        landmarks = iperrilles_landmarksapp(LandmarkName=form.Landmark_Name.data,City=form.City.data,Country=form.Country.data)
+        db.session.add(landmarks)
+        db.session.commit()
+        return redirect ('/')
 
     return render_template('add_Landmark.html', form=form, pageTitle='Add new Landmark')
 
