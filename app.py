661669213e127c4c71873cab61535f42b5afb510
iperrilles <isaac-perrilles@uiowa.edu>
2019-11-07 22:18:52 -0600

    Flask-WTF

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask
-from flask import render_template, request, redirect
+from flask import render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SuperSecretKey'
+
+class LandmarksForm(FlaskForm):
+    Landmark_Name = StringField('Landmark Name:', validators=[DataRequired()])
 
 @app.route('/')
 def index():
-    return render_template('index.html', display="", pageTitle='Index')
+    return render_template('index.html', display="", pageTitle='Landmarks')
+
+@app.route('/add_Landmark', methods=['GET', 'POST'])
+def add_Landmark():
+    form = LandmarksForm()
+    if form.validate_on_submit():
+        return "<h2> The Landmark is {0}".format(form.Landmark_Name.data)
+
+    return render_template('add_Landmark.html', form=form, pageTitle='Add new Landmark')
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
